8077303622a90942e3485666a15c3dca3a93bc9a
do.minh.hai <do.minh.hai@framgia.com>
2016-10-26 08:46:28 +0700

    update convention

--- practice1.rb
+++ practice1.rb
@@ -1,48 +1,37 @@
-#pivot is a value, this function return index of pivot in array
-def partition(arr,left,right)   
+# pivot is a value, this function return index of pivot in array
+def partition arr, left, right
 =begin
-using random pivot to optimize runtime
-    rand_index = rand(left..right)
-    pivot = arr[rand_index]
-    #swap pivot to end
-    arr[rand_index],arr[right] = arr[right],arr[rand_index]      
+# using random pivot to optimize runtime
+  rand_index = rand(left..right)
+  pivot = arr[rand_index]
+# swap pivot to end
+  arr[rand_index], arr[right] = arr[right], arr[rand_index]
 =end
     pivot = arr[right]
-    #puts "pivot:" + pivot.to_s
+    # puts "pivot:" + pivot.to_s
     i = left
-    for j in left..right-1
-    #puts "i:" + i.to_s
-    #puts "j:" + j.to_s
-        if arr[j] <= pivot  then 
-            #puts "array updated!"
-            arr[j],arr[i] = arr[i],arr[j]
-            i=i+1 
-            #puts arr.to_s
-        else
-            #puts arr[j].to_s
-            #puts "pivot:" + pivot
-            #puts "skip"
+    #for j in left..right-1 => remove for and use .each
+    (left..right-1).each{|j|
+        if arr[j] <= pivot
+            arr[j], arr[i] = arr[i], arr[j]
+            i += 1
         end
-    #puts "--------------"
-    end
-    #puts "array updated 2!"
-    arr[i],arr[right]=arr[right],arr[i]
-    #puts arr.to_s
-    #puts "pivot index:" + i.to_s
-    return i    
+    }
+    arr[i], arr[right] = arr[right], arr[i]
+    i
 end
 
-def quicksort(arr,left,right)       #From min to max
-    if(left < right)
-        pivot_index = partition(arr,left,right)
-        quicksort(arr,left,pivot_index-1)
-        quicksort(arr,pivot_index+1,right)
+def quicksort arr, left, right       # From min to max
+    if left < right
+        pivot_index = partition arr, left, right
+        quicksort arr, left, pivot_index-1
+        quicksort arr, pivot_index+1, right
     end
-    return arr
+    arr
 end
 
 arr = gets   # Input something like "1 2 3 4" or "3, 5, 6, 1"
-arr = arr.split(" ").map{|x| x.to_i}
+arr = arr.split(" ").map{|x| x.to_i }
 puts "input" + arr.to_s
-quicksort(arr,0,arr.length-1)
+quicksort arr, 0, arr.length-1
 puts "output" + arr.to_s
