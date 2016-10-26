34cba537d09a7b41896e2b378b0f8d56dff5ec42
do.minh.hai <do.minh.hai@framgia.com>
2016-10-26 19:01:54 +0700

    fix convention

--- merge_sort/spec/merge_sort_spec.rb
+++ merge_sort/spec/merge_sort_spec.rb
@@ -46,33 +46,33 @@ RSpec.describe 'Merge Sort' do
       let (:arr) { [3, 2] }
 
       it 'return sorted array' do
-        expect(merge_sort(arr)).to eq [2,3]
+        expect(merge_sort(arr)).to eq [2, 3]
 
       end
     end
     # multi elements array
     context 'multi elements array' do
       context 'increasing sorted array' do
-        let (:arr) {[1,2,5,9,123]}
+        let (:arr) {[1, 2, 5, 9, 123]}
 
         it 'return sorted array' do
-          expect(merge_sort arr).to eq [1,2,5,9,123]
+          expect(merge_sort arr).to eq [1, 2, 5, 9, 123]
         end
       end
 
       context 'decreasing sorted array' do
-        let (:arr) {[199,99,88,65,5,3,0,-1]}
+        let (:arr) {[199, 99, 88, 65, 5, 3, 0, -1]}
 
         it 'return sorted array' do
-          expect(merge_sort arr ).to eq [-1,0,3,5,65,88,99,199]
+          expect(merge_sort arr ).to eq [-1, 0, 3, 5, 65, 88, 99, 199]
         end
       end
 
       context 'uniform array' do
-        let (:arr) {[1,1,1,1,1,1]}
+        let (:arr) {[1, 1, 1, 1, 1, 1]}
 
         it 'return sorted array' do
-          expect(merge_sort arr ).to eq [1,1,1,1,1,1]
+          expect(merge_sort arr ).to eq [1, 1, 1, 1, 1, 1]
         end
       end
 
@@ -85,10 +85,10 @@ RSpec.describe 'Merge Sort' do
       end
 
       context 'random array' do
-        let (:arr) {[1,199,2,87,4,5]}
+        let (:arr) {[1, 199, 2, 87, 4, 5]}
 
         it 'return sorted array' do
-          expect(merge_sort arr ).to eq [1,2,4,5,87,199]
+          expect(merge_sort arr ).to eq [1, 2, 4, 5, 87, 199]
         end
       end
     end
