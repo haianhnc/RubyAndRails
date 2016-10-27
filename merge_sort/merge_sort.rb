d85be6db9ed4147bca53fb8c321d358fa5a3d7e0
Dzung Tran Xuan <xuandung.hp.vn@gmail.com>
2016-10-28 00:05:49 +0700

    Update merge_sort.rb

--- merge_sort/merge_sort.rb
+++ merge_sort/merge_sort.rb
@@ -1,9 +1,10 @@
 require 'pry'
+
 module MergeSort
-  def merge arr1, arr2
+  def merge(arr1, arr2)
     merged_arr = Array.new
-    # collect smaller element of 2 arrays until one of array become empty
-    while 0 < arr1.length && 0 < arr2.length do
+    
+    while arr1.length > 0 && arr2.length > 0 do
       if arr1[0] > arr2[0]
         merged_arr.push arr2.delete_at(0)
       else
@@ -11,26 +12,19 @@ module MergeSort
       end
     end
 
-    # shilf remaining array2 into merged_arr
-    arr2.each{|x| merged_arr << x } if arr2.length > 0
-    # shilf remaining array1 into merged_arr
-    arr1.each{|x| merged_arr << x } if arr1.length > 0
+    arr1.each { |x| merged_arr << x } if arr1.length > 0
+    arr2.each { |x| merged_arr << x } if arr2.length > 0
     
     merged_arr
   end
 
-
-  def merge_sort arr
+  def merge_sort(arr)
     throw ArgumentError unless arr.kind_of?(Array)
-    throw ArgumentError unless arr != nil
     return arr if arr.length < 2
 
-    arr1 = arr[0, arr.length/2]
-    arr2 = arr[arr.length/2,arr.length-1]
-    arr1 = merge_sort arr1
-    arr2 = merge_sort arr2
-    arr = merge arr1, arr2
-    arr
-  end
+    arr1 = merge_sort(arr[0, arr.length / 2])
+    arr2 = merge_sort(arr[arr.length / 2, arr.length - 1])
 
+    merge arr1, arr2
+  end
 end
